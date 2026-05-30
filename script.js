1e2e8fdb351cb953a1c4f3059671e74a79680b4d
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-30 20:06:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const time=document.getElementById('timer');
-setInterval(function () {
-	let now =new Date()
-	time.innerText=now.toLocaleString()
-},1000)
+// const time=document.getElementById('timer');
+// 	setInterval(function () {
+// 	let now =new Date()
+// 	time.innerText=now.toLocaleString()
+// },1000)
+const timer = document.getElementById("timer");
+
+function updateTimer() {
+    const now = new Date();
+    timer.textContent = now.toLocaleString();
+}
+
+updateTimer(); 
+setInterval(updateTimer, 1000);
